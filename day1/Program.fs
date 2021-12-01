e7a109d9ebe1061d837b81d20f5e9545b688e00a
Matthew Johnson (MRAI) <matjoh@microsoft.com>
2021-12-01 16:58:14 +0000

    Making this more idiomatic F#

--- day1/Program.fs
+++ day1/Program.fs
@@ -2,29 +2,28 @@ open System
 open System.IO
 
 
-let is_deeper (pair : int array) =
-    if (pair.[1] > pair.[0]) then 1
-    else 0
+let is_deeper (pair : int List) =
+    match pair with
+    | [a; b] when b > a -> 1
+    | _ -> 0
 
 
-let part1 (values : int seq) =
-    let depth_pairs = Seq.windowed 2 values
-    let sum = Seq.sumBy is_deeper depth_pairs
-    sum
+let part1 (values : int List) =
+    List.sumBy is_deeper (List.windowed 2 values)
 
 
-let part2 (values : int seq) =
-    let depth_triplets = Seq.windowed 3 values
-    let depth_sums = depth_triplets |> Seq.map(fun(window) -> Seq.sum window)
-    part1 depth_sums
+let part2 (values : int List) =
+    List.windowed 3 values
+        |> List.map(fun(window) -> List.sum window)
+        |> part1
 
 
 [<EntryPoint>]
 let main argv =
-    let lines = File.ReadLines(argv.[0])
-    let values = lines |> Seq.map(fun(o) -> o |> int)
-    let output1 = part1 values
-    printfn "Part 1: %d" output1
-    let output2 = part2 values
-    printfn "Part 2: %d" output2
+    let values = 
+        File.ReadLines(argv.[0])
+        |> Seq.map(fun(o) -> o |> int)
+        |> Seq.toList
+    printfn "Part 1: %d" (part1 values)
+    printfn "Part 2: %d" (part2 values)
     0
