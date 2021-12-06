b8630cbc302d959c0e9f52ebe008c56cadd4772f
Matthew Johnson (MSRC) <matjoh@microsoft.com>
2021-12-06 10:11:20 +0000

    Day6 complete

---
+++ day6/Program.fs
@@ -0,0 +1,46 @@
+open System
+open System.IO
+
+
+let reproduce (timeToSpawn, count) =
+    if timeToSpawn = 0 then
+        [(8, count); (6, count)]
+    else
+        [(timeToSpawn - 1, count)]
+
+let addFish (timeToSpawn, count0) (_, count1) =
+    (timeToSpawn, count0 + count1)
+
+let sumFish populations =
+    let pop = List.head populations
+    List.fold addFish pop (List.tail populations)
+
+
+let grow fish =
+    fish
+    |> List.map reproduce
+    |> List.concat
+    |> List.groupBy fst
+    |> List.map snd
+    |> List.map sumFish
+
+
+let rec simulate numDays fish =
+    if numDays = 0 then
+        List.sumBy snd fish
+    else
+        simulate (numDays - 1) (grow fish)   
+
+
+[<EntryPoint>]
+let main argv =
+    let fish = 
+        File.ReadAllText(argv.[0]).Split ','
+        |> Seq.map(int)
+        |> Seq.toList
+        |> List.countBy id
+        |> List.map (fun (timeToSpawn, count) -> (timeToSpawn, uint64(count)))
+
+    printfn "Part 1: %d" (simulate 80 fish)
+    printfn "Part 2: %d" (simulate 256 fish)
+    0
