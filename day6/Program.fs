d717dc271d86f8391f0f887b848ec0dc89b04e8d
Matthew Johnson (MSRC) <matjoh@microsoft.com>
2021-12-06 12:56:26 +0000

    Big improvement (using arrays)

--- day6/Program.fs
+++ day6/Program.fs
@@ -1,44 +1,39 @@
 open System.IO
 
 
-let reproduce (timeToSpawn, count) =
-    if timeToSpawn = 0 then
-        [(8, count); (6, count)]
-    else
-        [(timeToSpawn - 1, count)]
+let step (timeToSpawn, count) = (timeToSpawn - 1, count)
 
-let addFish (timeToSpawn, count0) (_, count1) =
-    (timeToSpawn, count0 + count1)
-
-let sumFish populations =
-    let pop = List.head populations
-    List.fold addFish pop (List.tail populations)
-
-
-let grow fish =
-    fish
-    |> List.map reproduce
-    |> List.concat
-    |> List.groupBy fst
-    |> List.map snd
-    |> List.map sumFish
+let grow (fish : uint64 array) =
+    [|
+        fish.[1];
+        fish.[2];
+        fish.[3];
+        fish.[4];
+        fish.[5];
+        fish.[6];
+        fish.[7] + fish.[0];
+        fish.[8];
+        fish.[0]
+    |]
 
 
 let rec simulate numDays fish =
     if numDays = 0 then
-        List.sumBy snd fish
+        Array.sum fish
     else
         simulate (numDays - 1) (grow fish)   
 
 
+let setCount fish (i, count) = Array.set fish i count
+
+
 [<EntryPoint>]
 let main argv =
-    let fish = 
-        File.ReadAllText(argv.[0]).Split ','
-        |> Seq.map(int)
-        |> Seq.toList
-        |> List.countBy id
-        |> List.map (fun (timeToSpawn, count) -> (timeToSpawn, uint64(count)))
+    let fish : uint64 array = Array.zeroCreate 9
+    File.ReadAllText(argv.[0]).Split ','
+    |> Seq.map(int)
+    |> Seq.countBy id
+    |> Seq.iter (fun (i, count) -> Array.set fish i (uint64(count)))
 
     printfn "Part 1: %d" (simulate 80 fish)
     printfn "Part 2: %d" (simulate 256 fish)
