541d521af745277354d0e59574765abef5260379
Matthew Johnson <matjoh@microsoft.com>
2021-12-27 20:23:36 -0700

    Cleanup for the final days

--- day24/Program.fs
+++ day24/Program.fs
@@ -1,10 +1,4 @@
-﻿//inp a - Read an input value and write it to variable a.
-//add a b - Add the value of a to the value of b, then store the result in variable a.
-//mul a b - Multiply the value of a by the value of b, then store the result in variable a.
-//div a b - Divide the value of a by the value of b, truncate the result to an integer, then store the result in variable a. (Here, "truncate" means to round the value toward zero.)
-//mod a b - Divide the value of a by the value of b, then store the remainder in variable a. (This is also called the modulo operation.)
-//eql a b - If the value of a and b are equal, then store the value 1 in variable a. Otherwise, store the value 0 in variable a.
-open System.IO
+﻿open System.IO
 
 
 type Value =
@@ -31,7 +25,7 @@ type Instruction =
 
 
 type State = {W: Value; X: Value; Y: Value; Z: Value;
-              InputIndex: int; Conditions: (Value * bool) list}
+              InputIndex: int}
 
 
 let parseValue value =
@@ -60,76 +54,53 @@ let parseInstruction (line : string) =
     | _ -> None
 
 
-let read register state =
-    let index = state.InputIndex
-    let newIndex = index + 1
+let read register state inputs =
+    let value = List.item state.InputIndex inputs
+    let newIndex = state.InputIndex + 1
     let someState =
         match register with
-        | W -> Some {state with W=Input index; InputIndex=newIndex}
-        | X -> Some {state with X=Input index; InputIndex=newIndex}
-        | Y -> Some {state with Y=Input index; InputIndex=newIndex}
-        | Z -> Some {state with Z=Input index; InputIndex=newIndex}
+        | W -> Some {state with W=Literal value; InputIndex=newIndex}
+        | X -> Some {state with X=Literal value; InputIndex=newIndex}
+        | Y -> Some {state with Y=Literal value; InputIndex=newIndex}
+        | Z -> Some {state with Z=Literal value; InputIndex=newIndex}
         | _ -> None
     
-    [Option.get someState]
+    Option.get someState
 
 
 let rec add a b =
     match a, b with
     | Literal 0L, y -> y
     | x, Literal 0L -> x
+    | Add (x, Literal y), Literal z -> Add (x, Literal (y + z))
     | Literal x, Literal y -> Literal (x + y)
-    | Add (Input x, Literal y), Literal z -> Add (Input x, Literal (y + z))
-    | Add (Literal x, Input y), Literal z -> Add (Input y, Literal (x + z))
-    | Add (w, Literal x), Add (y, Literal z) ->
-        Add ((add w y), (Literal (x + z)))
     | x, y -> Add (x, y)
 
 
 let rec multiply a b =
     match a, b with
-    | Literal 1L, y -> y
-    | x, Literal 1L -> x
-    | Literal 0L, _ -> Literal 0L
-    | _, Literal 0L -> Literal 0L
     | Literal x, Literal y -> Literal (x * y)
-    | Multiply (Input x, Literal y), Literal z -> Multiply (Input x, Literal (y * z))
-    | Multiply (Literal x, Input y), Literal z -> Multiply (Input y, Literal (x * z))
-    //| Add (x, y), Literal z -> Add (multiply x b, multiply y b)
     | x, y -> Multiply (x, y)
 
 
 let rec divide a b =
     match a, b with
-    | Literal 1L, y -> y
-    | x, Literal 1L -> x
     | Literal x, Literal y ->
-        Literal (int(float(x) / float(y)))
-    | Divide (Input x, Literal y), Literal z ->
-        Divide (Input x, Literal (y * z))
-    | Divide (Literal x, Input y), Literal _ ->
-        Divide (Literal x, multiply (Input y) b)
-    | Multiply (Input x, Literal y), Literal z when y % z = 0L ->
-        divide (Input x) (Literal (y / z))
+        Literal (int64(float(x) / float(y)))
     | x, y -> Divide (x, y)
 
 
 let rec modulo a b =
     match a, b with
     | Literal x, Literal y -> Literal (x % y)
-    | Modulo (_, Literal y), Literal z when y = z -> a
-    | Input _, Literal y when y > 9L -> a
-    | Add (Input _, Literal x), Literal y when x < y - 9L -> a
-    | Add (Multiply (_, Literal x), y), Literal z when x = z -> modulo y b
     | x, y -> Modulo (x, y)
 
 
 let equal a b =
     match a, b with
     | Literal x, Literal y -> Literal (if x = y then 1L else 0L)
-    | Input _, Literal y when y > 9L -> Literal 0L
-    | Literal x, Input _ when x > 9L -> Literal 0L
-    | Add (_, Literal y), Input _ when y > 8L -> Literal 0L
+    | Input _, Literal y when y > 9 || y = 0 -> Literal 0L
+    | Input _, Add (_, Literal y) when y > 9 -> Literal 0L
     | x, y -> Equal (x, y)
 
 
@@ -148,71 +119,88 @@ let load state value =
 
 
 let save state loc value =
-    match value with
-    | Equal (_, _) ->
-        [Literal 0L, (value, true); Literal 1L, (value, false)]
-        |> List.map (fun (outcome, condition) ->                
-            match loc with
-            | W -> Some {state with W=outcome; Conditions=condition :: state.Conditions}
-            | X -> Some {state with W=outcome; Conditions=condition :: state.Conditions}
-            | Y -> Some {state with W=outcome; Conditions=condition :: state.Conditions}
-            | Z -> Some {state with W=outcome; Conditions=condition :: state.Conditions}
-            | _ -> None)
-        |> List.map Option.get
-    | _ ->
-        let maybeState =
-            match loc with
-            | W -> Some {state with W=value}
-            | X -> Some {state with X=value}
-            | Y -> Some {state with Y=value}
-            | Z -> Some {state with Z=value}
-            | _ -> None
-        [Option.get maybeState]
+    let maybeState =
+        match loc with
+        | W -> Some {state with W=value}
+        | X -> Some {state with X=value}
+        | Y -> Some {state with Y=value}
+        | Z -> Some {state with Z=value}
+        | _ -> None
+
+    Option.get maybeState
 
 
 let rec valueToString value =
     match value with
     | Literal value -> sprintf "%d" value
     | Input index -> sprintf "x%d" index
-    | Add (a, b) -> sprintf "(%s + %s)" (valueToString a) (valueToString b)
-    | Multiply (a, b) -> sprintf "(%s * %s)" (valueToString a) (valueToString b)
-    | Divide (a, b) -> sprintf "(%s / %s)" (valueToString a) (valueToString b)
-    | Modulo (a, b) -> sprintf "(%s %% %s)" (valueToString a) (valueToString b)
-    | Equal (a, b) -> sprintf "(%s = %s)" (valueToString a) (valueToString b)
+    | Add (a, b) -> sprintf "%s + %s" (valueToString a) (valueToString b)
+    | Equal (a, b) -> sprintf "%s = %s" (valueToString a) (valueToString b)
     | _ -> ""
 
 
-let stateToString state =
-    sprintf "{\n\tW=%s;\n\tX=%s;\n\tY=%s;\n\tZ=%s\n}" (valueToString state.W) (valueToString state.X) (valueToString state.Y) (valueToString state.Z)
+let execute inputs state instruction =
+    match instruction with
+    | ReadOp reg ->
+        read reg state inputs
+    | AddOp (reg, value) ->
+        save state reg (add (load state reg) (load state value))
+    | MultiplyOp (reg, value) ->
+        save state reg (multiply (load state reg) (load state value))
+    | DivideOp (reg, value) ->
+        save state reg (divide (load state reg) (load state value))
+    | ModuloOp (reg, value) ->
+        save state reg (modulo (load state reg) (load state value))
+    | EqualOp (reg, value) ->
+        save state reg (equal (load state reg) (load state value))
 
 
-let execute states instruction =
-    for state in states do
-        printfn "%s\n%A" (stateToString state) instruction
-
-    states
-    |> List.collect (fun state ->
-        match instruction with
-        | ReadOp reg ->
-            read reg state
-        | AddOp (reg, value) ->
-            save state reg (add (load state reg) (load state value))
-        | MultiplyOp (reg, value) ->
-            save state reg (multiply (load state reg) (load state value))
-        | DivideOp (reg, value) ->
-            save state reg (divide (load state reg) (load state value))
-        | ModuloOp (reg, value) ->
-            save state reg (modulo (load state reg) (load state value))
-        | EqualOp (reg, value) ->
-            save state reg (equal (load state reg) (load state value)))
+let runProgram state inputs instructions =
+    List.fold (execute inputs) state instructions
 
 
-let runProgram state instructions =
-    List.fold execute [state] instructions
+let rec runDecompiled stack iteration conditions vals =
+    match vals with
+    | [] -> conditions
+    | (doPop, xval, yval) :: tail -> 
+        let w = Input iteration
+        let x = 
+            match List.tryHead stack with
+            | None -> xval
+            | Some value -> add value xval
+        let newStack = if doPop then List.tail stack else stack
+        match equal w x with
+        | Literal 0L ->
+            runDecompiled ((add w yval)::newStack) (iteration + 1L) conditions tail
+        | value ->
+            let newConditions = value :: conditions
+            runDecompiled newStack (iteration + 1L) newConditions tail    
+
+
+let rec inputsToString inputs =
+    match inputs with
+    | [] -> ""
+    | head :: tail -> head.ToString() + (inputsToString tail)
 
 
 [<EntryPoint>]
 let main argv =
+    let pops = [false; false; false; false; false; true; false;
+                true; true; false; true; true; true; true]
+    let xvals = [10; 12; 13; 13; 14; -2; 11;
+                 -15; -10; 10; -10; -4; -1; -1]
+    let yvals = [0; 6; 4; 2; 9; 1; 10;
+                 6; 4; 6; 3; 9; 15; 5]
+
+    let vals =
+        List.zip3 pops xvals yvals
+        |> List.map (fun (a, b, c) -> a, Literal b, Literal c)
+
+    printfn "Conditions:"
+    let conditions = runDecompiled [] 0 [] vals
+    for condition in conditions do
+        printfn "%s" (valueToString condition)
+
     let instructions =
         File.ReadAllLines argv.[0]
         |> Seq.map (parseInstruction >> Option.get)
@@ -224,7 +212,18 @@ let main argv =
         Y = Literal 0;
         Z = Literal 0;
         InputIndex = 0;
-        Conditions = [];
     }
-    let newState = runProgram state instructions
+
+    let part1 = [9L; 4L; 9L; 9L; 2L; 9L; 9L;
+                 4L; 1L; 9L; 5L; 9L; 9L; 8L]    
+    let part1State = runProgram state part1 instructions
+    assert (part1State.Z = Literal 0)
+    printfn "Part 1: %s" (inputsToString part1)
+    
+    let part2 = [2L; 1L; 1L; 9L; 1L; 8L; 6L;
+                 1L; 1L; 5L; 1L; 1L; 6L; 1L]
+    let part2State = runProgram state part2 instructions
+    assert (part2State.Z = Literal 0)
+    printfn "Part 2: %s" (inputsToString part2)
+
     0
---
+++ day25/Program.fs
@@ -0,0 +1,95 @@
+﻿open System.IO
+
+type SeaCucumber =
+    | East
+    | South
+
+
+let parseCucumber char =
+    match char with
+    | '>' -> Some East
+    | 'v' -> Some South
+    | _ -> None
+
+let eastOf cols (row, col) =
+    match col + 1 with
+    | east when east = cols -> (row, 0)
+    | east -> (row, east)
+
+let southOf rows (row, col) =
+    match row + 1 with
+    | south when south = rows -> (0, col)
+    | south -> (south, col)
+
+
+let moveEast cucumbers cols (point, cucumber) =
+    match cucumber with
+    | East ->
+        let east = eastOf cols point
+        match Map.tryFind east cucumbers with
+        | None -> Some (point, east, cucumber)
+        | Some _ -> None
+    | _ -> None
+
+
+let moveSouth cucumbers rows (point, cucumber) =
+    match cucumber with
+    | South ->
+        let south = southOf rows point
+        match Map.tryFind south cucumbers with
+        | None -> Some (point, south, cucumber)
+        | Some _ -> None
+    | _ -> None
+
+
+let move cucumbers (start, finish, cucumber) =
+    Map.change start (fun _ -> None) cucumbers
+    |> Map.change finish (fun _ -> Some cucumber)
+
+
+let rec simulate cucumbers rows cols steps =
+    let eastMoves =
+        Map.toList cucumbers
+        |> List.map (moveEast cucumbers cols)
+        |> List.filter Option.isSome
+        |> List.map Option.get
+
+    let eastMoved = List.fold move cucumbers eastMoves
+
+    let southMoves =
+        Map.toList eastMoved
+        |> List.map (moveSouth eastMoved rows)
+        |> List.filter Option.isSome
+        |> List.map Option.get
+
+    let newCucumbers = List.fold move eastMoved southMoves
+
+    match List.length eastMoves + List.length southMoves with
+    | 0 -> steps
+    | _ -> simulate newCucumbers rows cols (steps + 1)
+
+
+[<EntryPoint>]
+let main argv =
+    let seaFloor =
+        File.ReadAllLines argv.[0]
+        |> Seq.mapi (fun row line ->
+            line
+            |> Seq.mapi (fun col char ->
+                (row, col), char)
+            |> Seq.toList)
+        |> Seq.toList
+
+    let rows = List.length seaFloor
+    let cols = List.length (List.head seaFloor)
+
+    let cucumbers =
+        seaFloor
+        |> List.concat
+        |> List.map (fun (k, v) -> k, parseCucumber v)
+        |> List.filter (snd >> Option.isSome)
+        |> List.map (fun (k, v) -> k, Option.get v)
+        |> Map.ofList
+
+    printfn "Part 1: %d" (simulate cucumbers rows cols 1)
+    0
